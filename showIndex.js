3e02fb41e295a7dfb5ab54e1c3ca6d63eb940907
AvetisyanTatev <avetisyant125@gmail.com>
2024-07-24 17:14:08 +0400

    Show index

---
+++ showIndex.js
@@ -0,0 +1,22 @@
+let numList = [4, 7, 52, 11, 0, 8];
+let tmp = 0;
+for (let i = 0; i<numList.length; i++){
+    for(let j=0; j<numList.length-i; j++){
+        if(numList[j]>numList[j+1]){
+            tmp = numList[j];
+            numList[j] = numList[j+1];
+            numList[j+1]=tmp;
+            
+        }
+    }
+}
+console.log(numList);
+let number = +prompt(" Index is:  ");
+let index = -1;
+for (let i = 0; i<numList.length; i++){
+    if (numList[i] === number){
+        index =i
+        break;
+    }
+}
+console.log(index)
